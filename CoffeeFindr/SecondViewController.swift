4beab69ac7bf9b161e330c54cffb2c08955857e5
Matt Deuschle <mdeuschle@gmail.com>
2016-03-11 11:36:58 -0800

    Initial Commit

---
+++ CoffeeFindr/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CoffeeFindr
+//
+//  Created by Matt Deuschle on 3/11/16.
+//  Copyright © 2016 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
